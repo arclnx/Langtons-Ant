657e8d738870be402944c5e8dcd5b4b11ca91586
LoneSurvivr314 <LoneSurvivr314@users.noreply.github.com>
2021-02-02 19:27:34 -0700

    Corrected graphics

--- Langton's Ant.py
+++ Langton's Ant.py
@@ -1,8 +1,8 @@
 import pyglet
 from pyglet import shapes
 from pyglet import clock
-
-
+ 
+ 
 # Set up global variables
 WIDTH = 250
 HEIGHT = 250
@@ -12,15 +12,38 @@ GRID = [[COLORS[0] for y in range(HEIGHT)] for x in range(WIDTH)] # Fill Grid wi
 RULES = ['L', 'R']
 # Set up pyglet window
 window = pyglet.window.Window(WIDTH * SIZE, HEIGHT * SIZE)
-
+ 
+fps_display = pyglet.window.FPSDisplay(window)
+ 
+batch = pyglet.graphics.Batch()
+ 
+class Grid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+ 
+        self.rects = {}
+        for x in range(width):
+            self.rects[x] = {}
+            for y in range(height):
+                self.rects[x][y] = shapes.Rectangle(
+                        x * SIZE, 
+                        y * SIZE,
+                        SIZE,
+                        SIZE,
+                        color=COLORS[0],
+                        batch = batch)
+ 
+grid = Grid(WIDTH, HEIGHT)
+ 
 # Ant
 class Ant:
-    batch = pyglet.graphics.Batch()
     buffer = []
-
-    def __init__(self, x_pos, y_pos):
+ 
+    def __init__(self, x_pos, y_pos, grid):
         self.location = [x_pos, y_pos]
         self.direction = [1,0]
+        self.grid = grid
     
     def turn(self, direction):
         if direction == 'R':
@@ -41,28 +64,23 @@ class Ant:
                     (index + 1) % len(COLORS)]  # Set cell to next color
             self.turn(RULES[index])  # Turn
             self.move(1)  # Move
-            self.buffer.append(shapes.Rectangle(
-                        self.location[0] * SIZE, 
-                        self.location[1] * SIZE,
-                        SIZE,
-                        SIZE,
-                        color=GRID[self.location[0]][self.location[1]],
-                        batch = self.batch))
-
+            self.grid.rects[self.location[0]][self.location[1]].color = GRID[self.location[0]][self.location[1]]
+ 
     def debug(self):
         print('direction' + str(self.direction))
         print('location' + str(self.location))
-
-my_ant = Ant(100,100)
-
-def draw(dt):
-    Ant.batch.draw()
-    Ant.buffer = []
-
+ 
+my_ant = Ant(100,100, grid)
+ 
+@window.event
+def on_draw():
+    window.clear()
+    batch.draw()
+    fps_display.draw()
+ 
 def update(dt):
     my_ant.iterate(10)
    
-pyglet.clock.schedule_interval(update, 1.0/1000)
-pyglet.clock.schedule_interval(draw, 1.0/60)
-
+pyglet.clock.schedule_interval(update, 1/1000)
+ 
 pyglet.app.run()
